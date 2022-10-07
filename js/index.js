0174b9ccc8fa657fda097ce2b43ef70cd7004cfe
Aniraj1 <anirajshahi@gmail.com>
2022-10-07 14:15:10 +0545

    Footer & ScrollUp

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// console.log(window.getComputedStyle(document.documentElement).getPropertyValue('--div-background-color'));
+
+// a = window.getComputedStyle(document.documentElement).getPropertyValue('--div-background-color');
+
+// document.querySelector("body").style.backgroundColor = a;
+
+
+
+
+const toTop = document.querySelector(".scroll-up");
+
+window.addEventListener("scroll", scrollUp);
+
+function scrollUp() {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+}
+
+
+const nav = document.querySelector(".navbar");
+
+window.addEventListener("scroll", navUp);
+
+function navUp() {
+    if (window.pageYOffset > 750) {
+        nav.classList.add("visible");
+    } else {
+        nav.classList.remove("visible");
+    }
+}
